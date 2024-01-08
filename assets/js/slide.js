2e92f5a98805866c9a1302f3af5727048cde38f0
ztrackperu <ztrack@zgroup.com.pe>
2024-01-08 15:56:47 -0500

    pagina base

---
+++ assets/js/slide.js
@@ -0,0 +1,48 @@
+var swiper = new Swiper('.swiper-container',{
+    slidesPerView: 1,
+    
+    // loop: true,
+    effect:'coverflow',
+    grabCursor:true,
+    centeredSlides:true,
+    slidesPerView:'auto',
+    coverflowEffect:{
+rotate:20,
+stretch:0,
+depth:1500,
+modifier:1,
+slideShadows:true,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+});
+
+
+
+
+
+var swiper = new Swiper(".mySwiper", {
+   
+    centeredSlides: true,
+    loop:true,
+    effect: "fade",
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
